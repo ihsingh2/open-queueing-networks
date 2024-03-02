ef938aa0e7c2e466b5614db480f8f5b52f4b47c3
ihsingh2 <ihsingh2@gmail.com>
2024-03-02 13:12:19 +0530

    final touchup

--- simulation/case3.py
+++ simulation/case3.py
@@ -1,14 +1,15 @@
 from network import Network, Job
 
-R = [0.5, 0]
-S = [3, 3]
+R = [20, 10, 0]
+S = [30, 30, 50]
 P = [
-    [0.1, 0.9],
-    [0, 0.2],
+    [0, 0, 1],
+    [0, 0, 1],
+    [0, 0.1, 0]
 ]
 
 N = Network(R, S, P, 0.01)
-N.simulate(15000, 0.001)
+N.simulate(5000, 0.001)
 
 N.print_stats()
 N.plot_stats()
